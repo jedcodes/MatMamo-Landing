1be1e56e4c6c4b59f729f3a20c1ff22756ea7f5e
Jack Delamou <jackdelamou.97@gmail.com>
2024-04-12 14:44:45 +0200

    Update Features.tsx

--- matmamo-app/components/Features.tsx
+++ matmamo-app/components/Features.tsx
@@ -9,7 +9,7 @@ const Features = () => {
       <div className="max-container padding-container relative w-full flex justify-end">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image
-            src="/phone.png"
+            src="/hero-noBg.png"
             alt="phone"
             width={440}
             height={1000}
